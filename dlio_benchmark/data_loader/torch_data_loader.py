2752265026701a7fdd198ef370f9941ad98143c1
Bernard Han <bernardhan@google.com>
2024-03-07 17:39:48 +0000

    GCS baseline: run against 150MB-750GB

--- dlio_benchmark/data_loader/torch_data_loader.py
+++ dlio_benchmark/data_loader/torch_data_loader.py
@@ -155,7 +155,7 @@ class TorchDataLoader(BaseDataLoader):
             bucket_name="dataflux-tessellations-datasets",
             data_format_fn=read_image_modified,
             config=dataflux_mapstyle_dataset.Config(
-                prefix="UNet3D/medium/3MB-150GB/train",
+                prefix="UNet3D/large/150MB-750GB/train",
                 num_processes=1,
                 max_composite_object_size=0,
             ),
