fa2828afcad33086123508bce96e8f88ecf637f6
Bernard Han <bernardhan@google.com>
2024-03-15 20:51:15 +0000

    analyze batch data

--- parse_and_report.py
+++ parse_and_report.py
@@ -38,11 +38,11 @@ if __name__ == "__main__":
 
         data = json.load(f)
 
-        epochs1.append(int(data["1"]["duration"]))
-        epochs2.append(int(data["2"]["duration"]))
-        epochs3.append(int(data["3"]["duration"]))
-        epochs4.append(int(data["4"]["duration"]))
-        epochs5.append(int(data["5"]["duration"]))
+        epochs1.append(float(data["1"]["duration"]))
+        epochs2.append(float(data["2"]["duration"]))
+        epochs3.append(float(data["3"]["duration"]))
+        epochs4.append(float(data["4"]["duration"]))
+        epochs5.append(float(data["5"]["duration"]))
         f.close()
 
     print(len(epochs1))
